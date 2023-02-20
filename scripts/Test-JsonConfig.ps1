0c0e3a27122c82c0819de1d8bc15d9ed29f03eea
Georgi Slavov <external.slavov_georgi@allianzdirect.de>
2023-02-20 15:52:42 +0200

    test

---
+++ scripts/Test-JsonConfig.ps1
@@ -0,0 +1,41 @@
+[CmdletBinding()]
+param (
+    [Parameter()]
+    [string] $Environment = 'dev'
+)
+
+process {
+    $validationErrors = [System.Collections.Generic.List[string]]::new()
+
+    #Enumerate json files
+
+    #Valid JSON
+    $repoRootFolder = Split-Path -Path $PSScriptRoot -Parent
+    $testConfigPath = Join-Path -Path $repoRootFolder -ChildPath 'config' -AdditionalChildPath $Environment, 'test.json'
+    $testConfig = Get-Content -Path $testConfigPath -Raw
+    $testConfigSchemaFilePath = Join-Path -Path $PSScriptRoot -ChildPath 'jobConfig.schema.json'
+    $testConfigSchema = Get-Content -Path $testConfigSchemaFilePath -Raw
+
+    try {
+        #Validate against schema
+        $null = Test-Json -Json $testConfig -Schema $testConfigSchema -ErrorAction Stop
+    }
+    catch {
+        $validationErrors.Add("Provided JSON does not pass schema is not valid. Details: $_")
+    }
+
+
+    $testConfig = $testConfig | ConvertTo-Json -ErrorAction Stop
+    $testNumber = 0
+    foreach ($test in $testConfig) {
+        if ($test.name -in @('import-csv', 'load-from-baselayer')) {
+            $schedule = $test.schedule
+
+            if ($null -eq $schedule) {
+                $validationErrors.Add("Provided JSON does not pass schema is not valid. Details: Schedule parameter is missing #/[$testNumber]")
+            }
+        }
+
+        $testNumber++
+    }
+}
